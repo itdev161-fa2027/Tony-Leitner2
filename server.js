3c4157d542019dc51161fa7261d246d5ca043fb5
Tony Leitner Jr <leitnet2@matc.edu>
2026-09-15 17:16:42 -0500

    completed activity 3

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 import express from "express";
+import { check, validationResult } from "express-validator";
 import connectDatabase from "./config/db.js";
 
 const app = express();
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
 app.get("/", (request, response) => {
-  response.send("http get rrequest sent to root api endpoint.");
+  response.send("HTTP GET request sent to root API endpoint.");
 });
+
+// User registration endpoint
+app.post(
+  "/api/users",
+  [
+    check("name", "Name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must be password123").equals("password123")
+  ],
+  (request, response) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    response.json(request.body);
+  }
+);
 
 connectDatabase()
   .then(() => {
